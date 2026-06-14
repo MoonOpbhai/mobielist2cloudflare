6799939900c70de334e850b99ce88b35d78f9646
Moon Fuego <mooncarder0@gmail.com>
2026-06-14 22:14:20 +0530

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,4 +1,5 @@
 window.APP_CONFIG = {
   SUPABASE_URL: "https://jkvldbtwpfudiccgqahu.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_FwswD-lkG6NhoCREWUS9Lg_zY_KBgNE"
+  SUPABASE_ANON_KEY: "sb_publishable_FwswD-lkG6NhoCREWUS9Lg_zY_KBgNE",
+  TMDB_API_KEY: "05cc08d507ec64e49baa371743d450cf"
 };
