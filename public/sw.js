cfb681969d8634c58c58682eaf21d6f9578ad430
Moon Fuego <mooncarder0@gmail.com>
2026-06-18 16:47:12 +0530

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'moon-flix-shell-v1';
+const SHELL = ['./', './style.css?v=moon-ui-v28', './app.js?v=moon-ui-v28', './config.js?v=moon-ui-v28'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(SHELL)).catch(() => {})
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Network-first: always try fresh data, fall back to cached shell if offline
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(event.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
