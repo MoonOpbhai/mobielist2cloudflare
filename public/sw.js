6ddf5ade782bba82a15ab27b09c65f85cf45eb47
Moon Fuego <mooncarder0@gmail.com>
2026-06-30 12:59:16 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,8 @@
-const CACHE = 'moon-flix-shell-v15';
+const CACHE = 'moon-flix-shell-v16';
 const SHELL = [
-  './style.css?v=moon-ui-v45',
-  './app.js?v=moon-ui-v45',
-  './config.js?v=moon-ui-v45',
+  './style.css?v=moon-ui-v46',
+  './app.js?v=moon-ui-v46',
+  './config.js?v=moon-ui-v46',
   './icon-192.png?v=2',
   './icon-512.png?v=2'
 ];
